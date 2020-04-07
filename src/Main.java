c6b36f4071eac9672bfd2109cab7b2237fe70185
Matthew Toomey <mtoomey2@worcester.edu>
2020-04-07 13:00:21 -0400

    applied clean code principles

--- src/Main.java
+++ src/Main.java
@@ -4,49 +4,61 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to SpaghettiSim.");
-        String nm;
-        int a;
-        String p;
-        String spaghetti;
-        String spaghetti2;
-        Scanner s = new Scanner(System.in);
+        System.out.println("Welcome.");
 
-        System.out.println("Enter your name:");
-        nm = s.nextLine();
-        System.out.println("Enter a number:");
-        a = Integer.parseInt(s.nextLine());
-        System.out.println("Enter a pasta:");
-        p = s.nextLine();
-        System.out.println("Enter a spaghetti dish:");
-        spaghetti = s.nextLine();
-        System.out.println("Enter another spaghetti dish:");
-        spaghetti2 = s.nextLine();
+        // used to personalize user fortune based on input
+        String name;
+        String pastaType;
+        String pastaMeal;
+        String spaghettiMeal;
 
-        System.out.println("Thank you.");
-        System.out.println("Name: " + nm);
-        System.out.println("Number: " + a);
-        System.out.println("Pasta type: " + p);
-        System.out.println("First spaghetti dish: "  + spaghetti);
-        System.out.println("Second spaghetti dish: " + spaghetti2);
+        name = getUserInput("Enter your name: ");
+        pastaType = getUserInput("Enter your favorite pasta: " );
+        pastaMeal = getUserInput("Enter a meal with pasta: ");
+        spaghettiMeal = getUserInput("Enter a meal with spaghetti: ");
 
-        Random r = new Random();
-        String[] spaghettiScopes;
-        spaghettiScopes = new String[10];
-        spaghettiScopes[0] = "Based on the " + p + " and " + spaghetti + ", you will have great luck tomorrow.";
-        spaghettiScopes[1] = "Based on the " + p + " and " + spaghetti + ", you will go skiing tomorrow.";
-        spaghettiScopes[2] = "Based on the " + p + " and " + spaghetti + ", you will soon watch a bad movie.";
-        spaghettiScopes[3] = "Based on the " + p + " and " + spaghetti + ", you will encounter a bird.";
-        spaghettiScopes[4] = "Based on the " + p + " and " + spaghetti + ", you will find treasure.";
-        spaghettiScopes[5] = "Based on the " + p + " and " + spaghetti + ", you will soon encounter lots of " + spaghetti2 + ".";
-        spaghettiScopes[6] = "Based on the " + p + " and " + spaghetti + ", you will play a board game soon.";
-        spaghettiScopes[7] = "Based on the " + p + " and " + spaghetti + ", rain will soon fall from the sky.";
-        spaghettiScopes[8] = "Based on the " + p + " and " + spaghetti + ", Amazon will have a sale on "  + p + ".";
-        spaghettiScopes[9] = "Based on the " + p + " and " + spaghetti + ", you will find great happiness and " + spaghetti + ".";
+        System.out.println("Spaghetti Statistics");
+        System.out.println("Name: " + name);
+        System.out.println("Pasta type: " + pastaType);
+        System.out.println("Pasta meal: "  + pastaMeal);
+        System.out.println("Spaghetti meal: " + spaghettiMeal +"\n");
 
-        int mySP = r.nextInt(10);
-        System.out.println(spaghettiScopes[mySP]);
-        System.out.println("Thank you for using spaghettiSim.");
+        String[] fortunes;
+        fortunes = new String[10];
+        // create the 10 fortunes to choose from when returning the user's fortune
+        fortunes[0] = "Based on the " + pastaType + " and " + pastaMeal + ", you will have great luck tomorrow.";
+        fortunes[1] = "Based on the " + pastaType + " and " + spaghettiMeal + ", you will go skiing tomorrow.";
+        fortunes[2] = "Based on the " + pastaType + " and " + pastaMeal + ", you will soon watch a bad movie.";
+        fortunes[3] = "Based on the " + pastaType + " and " + spaghettiMeal + ", you will encounter a bird.";
+        fortunes[4] = "Based on the " + pastaType + " and " + pastaMeal + ", you will find treasure.";
+        fortunes[5] = "Based on the " + pastaType + " and " + pastaMeal + ", you will soon encounter lots of " + spaghettiMeal + ".";
+        fortunes[6] = "Based on the " + pastaType + " and " + spaghettiMeal + ", you will play a board game soon.";
+        fortunes[7] = "Based on the " + pastaType + " and " + pastaMeal + ", rain will soon fall from the sky.";
+        fortunes[8] = "Based on the " + pastaType + " and " + spaghettiMeal + ", Amazon will have a sale on "  + pastaType + ".";
+        fortunes[9] = "Based on the " + pastaType + " and " + spaghettiMeal + ", you will find great happiness and " + pastaMeal + ".";
 
+        readUserFortune(fortunes);
+
+        // Copyright (C) Matthew Toomey 2020
+    }
+
+    public static String getUserInput(String prompt)
+    {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println(prompt);
+        String output = userInput.nextLine();
+
+        return output;
+    }
+
+    public static void readUserFortune(String[] fortunesArray)
+    {
+        Random randomFortune = new Random();
+        int fortuneChoice = randomFortune.nextInt(10);
+
+        System.out.println(fortunesArray[fortuneChoice]);
+
+        System.out.println("Have a nice day.");
     }
 }
